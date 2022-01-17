c6f68704fd144ddd76dda1348586d6f359ce56b8
RaakiDP787 <raaki.dp@gmail.com>
2022-01-17 13:29:18 +0530

    Example Gradle-Groovy project
    Example execution and demostration of custom task in gradle and one groovy project

---
+++ Example.groovy
@@ -0,0 +1,6 @@
+class Example {
+    static void main(String[] args) {
+        // Example of groovy function to execute the print one statement
+        println('Example of groovy program');
+    }
+}
